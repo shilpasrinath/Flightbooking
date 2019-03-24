6d1e358538af173e01ede7bfdca35a01b293736a
shilpasrinath <shilpasrinath71@gmail.com>
2019-03-24 15:06:23 +0000

    committed

--- src/test/java/Runner/Testrunner.java
+++ src/test/java/Runner/Testrunner.java
@@ -8,11 +8,11 @@ import cucumber.api.junit.Cucumber;
 
 	@RunWith(Cucumber.class)
 	@CucumberOptions(features="C:\\Users\\srinath\\eclipse-workspace\\Flightbooking\\src\\main\\java\\Feature\\flightbooking.feature",//path of featurefile
-	              glue= {"Stepdefinition"},//path of stepdefinition
-	             format= {"pretty", "html:test-output","json:json-output/cucumber.json","junit:junit_xml/cucumber.xml"},//generate differet types of reporting
-	               dryRun=false,//when true it wont execute but checks correct mapping between feature and stepdefinition file
-	               monochrome=true,//display the console output in pretty readable format
-	               strict=true//it will check if any step is not defined in step definition it throws pending exception
+	              glue= {"Stepdefinition"},
+	              format= {"pretty", "html:test-output","json:json-output/cucumber.json","junit:junit_xml/cucumber.xml"},
+	               dryRun=false,
+	               monochrome=true,
+	               strict=true
 	              )
 	     public class Testrunner {
 		
